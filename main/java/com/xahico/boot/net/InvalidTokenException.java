1aeaaf68a0bacfec2630a956e95f4df4f1b8ad67
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:12:57 +0300

    add: main/java/com/xahico/boot/net/InvalidTokenException.java

---
+++ main/java/com/xahico/boot/net/InvalidTokenException.java
@@ -0,0 +1,17 @@
+/*
+ * Written by Tuomas Kontiainen <https://www.github.com/tutomiko>
+ * 
+ * Copyright (c) 2023, Tuomas Kontiainen
+ */
+package com.xahico.boot.net;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public class InvalidTokenException extends Exception {
+	public InvalidTokenException (){
+		super();
+	}
+}
