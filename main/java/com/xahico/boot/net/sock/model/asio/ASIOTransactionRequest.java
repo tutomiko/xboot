177090cd776f3e572ac6a2f2d60ab4c01f9c44a1
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:13:44 +0300

    add: main/java/com/xahico/boot/net/sock/model/asio/ASIOTransactionRequest.java

---
+++ main/java/com/xahico/boot/net/sock/model/asio/ASIOTransactionRequest.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.xahico.boot.net.sock.model.asio;
+
+import com.xahico.boot.lang.jsox.JSOXObject;
+import com.xahico.boot.lang.jsox.JSOXVariant;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+final class ASIOTransactionRequest extends JSOXObject {
+	public String      control;
+	public JSOXVariant data;
+	public int         tid;
+}
