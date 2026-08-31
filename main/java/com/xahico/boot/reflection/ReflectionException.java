e84100db143721335860be5fb2100513727534e3
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:22:54 +0300

    add: main/java/com/xahico/boot/reflection/ReflectionException.java

---
+++ main/java/com/xahico/boot/reflection/ReflectionException.java
@@ -0,0 +1,24 @@
+/*
+ * Written by Tuomas Kontiainen <https://www.github.com/tutomiko>
+ * 
+ * Copyright (c) 2023, Tuomas Kontiainen
+ */
+package com.xahico.boot.reflection;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public class ReflectionException extends Exception {
+	/**
+	 * Constructs an instance of <code>ReflectionException</code> with the
+	 * specified detail message.
+	 *
+	 * @param msg 
+	 * the detail message.
+	**/
+	public ReflectionException (final String msg){
+		super(msg);
+	}
+}
