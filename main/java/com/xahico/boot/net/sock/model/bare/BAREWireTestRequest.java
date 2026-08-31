5788e9158eabce074fa2f8c8179c7a2b4f7804ff
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:14:23 +0300

    add: main/java/com/xahico/boot/net/sock/model/bare/BAREWireTestRequest.java

---
+++ main/java/com/xahico/boot/net/sock/model/bare/BAREWireTestRequest.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.xahico.boot.net.sock.model.bare;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+final class BAREWireTestRequest extends BARERequest {
+	public long heartbeat;
+}
