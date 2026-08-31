76201308e1f06bf722d8e9b6a0181c081c4cf8a4
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:09:20 +0300

    add: main/java/com/xahico/boot/io/IOBufferBase.java

---
+++ main/java/com/xahico/boot/io/IOBufferBase.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.xahico.boot.io;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public abstract class IOBufferBase implements IOBuffer {
+	protected IOBufferBase (){
+		super();
+	}
+}
