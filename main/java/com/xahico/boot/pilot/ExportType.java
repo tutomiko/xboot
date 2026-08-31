1b8990029533f106f2b1e3c0d92422358a36a855
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:19:14 +0300

    add: main/java/com/xahico/boot/pilot/ExportType.java

---
+++ main/java/com/xahico/boot/pilot/ExportType.java
@@ -0,0 +1,17 @@
+/*
+ * Written by Tuomas Kontiainen <https://www.github.com/tutomiko>
+ * 
+ * Copyright (c) 2023, Tuomas Kontiainen
+ */
+package com.xahico.boot.pilot;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public enum ExportType {
+	START,
+	
+	STOP,
+}
