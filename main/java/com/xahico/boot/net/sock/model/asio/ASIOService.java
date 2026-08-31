940c88a18be0fc041528533b8707405c03748cf5
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:13:34 +0300

    add: main/java/com/xahico/boot/net/sock/model/asio/ASIOService.java

---
+++ main/java/com/xahico/boot/net/sock/model/asio/ASIOService.java
@@ -0,0 +1,32 @@
+/*
+ * Written by Tuomas Kontiainen <https://www.github.com/tutomiko>
+ * 
+ * Copyright (c) 2023, Tuomas Kontiainen
+ */
+package com.xahico.boot.net.sock.model.asio;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import com.xahico.boot.pilot.ServiceType;
+
+/**
+ * Buffered Abstract Session Handler.
+ * 
+ * @author Tuomas Kontiainen
+**/
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@ServiceType(ASIOServiceProvider.class)
+public @interface ASIOService {
+	int bufferSize () default 512;
+	
+	ASIOOption[] options () default {};
+	
+	int port () default 0;
+	
+	boolean useDynamicBuffers () default false;
+}
