5df8330417613727bf5db76f5d20913efa290bd0
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:25:55 +0300

    add: main/resources/module-inf.java

---
+++ main/resources/module-inf.java
@@ -0,0 +1,80 @@
+/*
+ * Written by Tuomas Kontiainen <https://www.github.com/tutomiko>
+ * 
+ * Copyright (c) 2023, Tuomas Kontiainen
+ */
+/*
+module com.xahico.boot {
+	requires io.netty.common;
+	requires io.netty.buffer;
+	requires io.netty.codec.http;
+	requires io.netty.handler;
+	requires io.netty.transport;
+	requires java.net.http;
+	requires jdk.httpserver;
+	requires jdk.management;
+	requires org.json;
+	
+	opens com.xahico.boot;
+	opens com.xahico.boot.algorithm;
+	opens com.xahico.boot.collections;
+	opens com.xahico.boot.cryptography;
+	opens com.xahico.boot.event;
+	opens com.xahico.boot.geo;
+	opens com.xahico.boot.io;
+	opens com.xahico.boot.lang.html;
+	opens com.xahico.boot.lang.json;
+	opens com.xahico.boot.lang.jsox;
+	opens com.xahico.boot.lang.xml;
+	opens com.xahico.boot.logging;
+	opens com.xahico.boot.net;
+	opens com.xahico.boot.net.inet;
+	opens com.xahico.boot.net.sock;
+	opens com.xahico.boot.net.sock.model.bare;
+	opens com.xahico.boot.net.sock.model.bash;
+	opens com.xahico.boot.net.sock.model.cortex;
+	opens com.xahico.boot.net.sock.model.lobex;
+	opens com.xahico.boot.net.sock.model.trax;
+	opens com.xahico.boot.net.web;
+	opens com.xahico.boot.net.web.http;
+	opens com.xahico.boot.net.web.http.model.glass;
+	opens com.xahico.boot.pilot;
+	opens com.xahico.boot.platform;
+	opens com.xahico.boot.synchronicity;
+	opens com.xahico.boot.util;
+	opens com.xahico.boot.util.async;
+	opens com.xahico.boot.util.configurable;
+	opens com.xahico.boot.util.transformer;
+	
+	exports com.xahico.boot.algorithm;
+	exports com.xahico.boot.collections;
+	exports com.xahico.boot.cryptography;
+	exports com.xahico.boot.event;
+	exports com.xahico.boot.geo;
+	exports com.xahico.boot.io;
+	exports com.xahico.boot.lang.html;
+	exports com.xahico.boot.lang.json;
+	exports com.xahico.boot.lang.jsox;
+	exports com.xahico.boot.lang.xml;
+	exports com.xahico.boot.logging;
+	exports com.xahico.boot.net;
+	exports com.xahico.boot.net.inet;
+	exports com.xahico.boot.net.sock;
+	exports com.xahico.boot.net.sock.model.bare;
+	exports com.xahico.boot.net.sock.model.bash;
+	exports com.xahico.boot.net.sock.model.cortex;
+	exports com.xahico.boot.net.sock.model.lobex;
+	exports com.xahico.boot.net.sock.model.trax;
+	exports com.xahico.boot.net.web;
+	exports com.xahico.boot.net.web.http;
+	exports com.xahico.boot.net.web.http.model.glass;
+	exports com.xahico.boot.pilot;
+	exports com.xahico.boot.platform;
+	exports com.xahico.boot.accessibility.search;
+	exports com.xahico.boot.synchronicity;
+	exports com.xahico.boot.util;
+	exports com.xahico.boot.util.async;
+	exports com.xahico.boot.util.configurable;
+	exports com.xahico.boot.util.transformer;
+}
+*/
