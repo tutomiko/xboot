2090660974fbda20e9c10dface531c0e58fb280e
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:18:58 +0300

    add: main/java/com/xahico/boot/net/web/WebServiceProvider.java

---
+++ main/java/com/xahico/boot/net/web/WebServiceProvider.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.xahico.boot.net.web;
+
+import com.xahico.boot.net.sock.TCPServiceProvider;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public abstract class WebServiceProvider extends TCPServiceProvider {
+	protected WebServiceProvider (){
+		super();
+	}
+}
