0b2a41a0be49708298b1605bfb8b4dd20fe3332f
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:24:36 +0300

    add: main/java/com/xahico/boot/util/ListBuilder.java

---
+++ main/java/com/xahico/boot/util/ListBuilder.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.xahico.boot.util;
+
+import java.util.List;
+
+/**
+ * TBD.
+ * 
+ * @param <T> 
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public interface ListBuilder <T> {
+	List<T> build ();
+}
