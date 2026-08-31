b265d18280e126ee3163ae60ea6ee768c44e683c
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:17:31 +0300

    add: main/java/com/xahico/boot/net/web/http/HttpServiceProviderBase.java

---
+++ main/java/com/xahico/boot/net/web/http/HttpServiceProviderBase.java
@@ -0,0 +1,19 @@
+/*
+ * Written by Tuomas Kontiainen <https://www.github.com/tutomiko>
+ * 
+ * Copyright (c) 2023, Tuomas Kontiainen
+ */
+package com.xahico.boot.net.web.http;
+
+import com.xahico.boot.net.web.WebServiceProvider;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public abstract class HttpServiceProviderBase extends WebServiceProvider {
+	protected HttpServiceProviderBase (){
+		super();
+	}
+}
