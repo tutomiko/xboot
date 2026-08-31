cbcbee045ed30861b58313e4021fdc2cb97d1957
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:25:03 +0300

    add: main/java/com/xahico/boot/util/PseudoEnum.java

---
+++ main/java/com/xahico/boot/util/PseudoEnum.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.xahico.boot.util;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public abstract class PseudoEnum {
+	protected PseudoEnum (){
+		super();
+	}
+	
+	
+	
+	public abstract String name ();
+}
