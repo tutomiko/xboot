0a00f1d261645dccf3790ea571ca938ccd9fd2fd
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:17:01 +0300

    add: main/java/com/xahico/boot/net/web/http/HttpMethod.java

---
+++ main/java/com/xahico/boot/net/web/http/HttpMethod.java
@@ -0,0 +1,21 @@
+/*
+ * Written by Tuomas Kontiainen <https://www.github.com/tutomiko>
+ * 
+ * Copyright (c) 2023, Tuomas Kontiainen
+ */
+package com.xahico.boot.net.web.http;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public enum HttpMethod {
+	GET,
+	
+	OPTIONS,
+	
+	POST,
+	
+	PUT,
+}
