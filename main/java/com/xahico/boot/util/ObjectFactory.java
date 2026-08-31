715cf215c5aef5a5a988472e1766080bf18b77fd
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:24:46 +0300

    add: main/java/com/xahico/boot/util/ObjectFactory.java

---
+++ main/java/com/xahico/boot/util/ObjectFactory.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.xahico.boot.util;
+
+/**
+ * TBD.
+ * 
+ * @param <T> 
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+@FunctionalInterface
+public interface ObjectFactory <T> {
+	T newInstance ();
+}
