0bdc97645f634e3bc90857f5812903e55e53a8a5
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:15:07 +0300

    add: main/java/com/xahico/boot/net/sock/model/cortex/CORTEXService.java

---
+++ main/java/com/xahico/boot/net/sock/model/cortex/CORTEXService.java
@@ -0,0 +1,32 @@
+/*
+ * Written by Tuomas Kontiainen <https://www.github.com/tutomiko>
+ * 
+ * Copyright (c) 2023, Tuomas Kontiainen
+ */
+package com.xahico.boot.net.sock.model.cortex;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import com.xahico.boot.pilot.ServiceType;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@ServiceType(CORTEXServiceProvider.class)
+public @interface CORTEXService {
+	int bufferSize () default 512;
+	
+	Class<? extends CORTEXEvent> eventClass ();
+	
+	String host ();
+	
+	int port ();
+}
