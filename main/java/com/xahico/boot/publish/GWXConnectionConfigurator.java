4b91f697def916438d20bd56fb45cada0746d7e9
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:21:07 +0300

    add: main/java/com/xahico/boot/publish/GWXConnectionConfigurator.java

---
+++ main/java/com/xahico/boot/publish/GWXConnectionConfigurator.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.xahico.boot.publish;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+@FunctionalInterface
+public interface GWXConnectionConfigurator {
+	void configure (final GWXConnectionConfiguration config);
+}
