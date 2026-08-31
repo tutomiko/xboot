5cb64724a6d2e974053829a188c683955f56804d
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:11:01 +0300

    add: main/java/com/xahico/boot/lang/javascript/JSNamedObject.java

---
+++ main/java/com/xahico/boot/lang/javascript/JSNamedObject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.xahico.boot.lang.javascript;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public abstract class JSNamedObject extends JSObject {
+	public JSNamedObject (){
+		super();
+	}
+	
+	
+	
+	public abstract String getName ();
+}
