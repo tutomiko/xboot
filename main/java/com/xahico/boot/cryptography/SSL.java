999c9e517f3e2f9b1f02d06c6d56203f6db5d5c2
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:08:49 +0300

    add: main/java/com/xahico/boot/cryptography/SSL.java

---
+++ main/java/com/xahico/boot/cryptography/SSL.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.xahico.boot.cryptography;
+
+import javax.net.ssl.SSLContext;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public interface SSL {
+	SSLContext createSSLContext ();
+}
