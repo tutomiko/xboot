95898ed5411b47ba4e00a4bc4bb8bd60a35ebb3d
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:18:11 +0300

    add: main/java/com/xahico/boot/net/web/http/model/glass/GlassImportable.java

---
+++ main/java/com/xahico/boot/net/web/http/model/glass/GlassImportable.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.xahico.boot.net.web.http.model.glass;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public interface GlassImportable {
+	String get (final String key);
+}
