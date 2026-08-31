926285fce2f331c90b1111759892abef48a5a019
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:22:05 +0300

    add: main/java/com/xahico/boot/publish/GWXProperty.java

---
+++ main/java/com/xahico/boot/publish/GWXProperty.java
@@ -0,0 +1,26 @@
+/*
+ * Written by Tuomas Kontiainen <https://www.github.com/tutomiko>
+ * 
+ * Copyright (c) 2023, Tuomas Kontiainen
+ */
+package com.xahico.boot.publish;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface GWXProperty {
+	boolean id () default false;
+	
+	String key () default "";
+}
