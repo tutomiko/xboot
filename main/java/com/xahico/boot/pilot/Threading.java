c4ba17a6b89eb7e712e703d9ade5014b2b5628b5
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:19:59 +0300

    add: main/java/com/xahico/boot/pilot/Threading.java

---
+++ main/java/com/xahico/boot/pilot/Threading.java
@@ -0,0 +1,27 @@
+/*
+ * Written by Tuomas Kontiainen <https://www.github.com/tutomiko>
+ * 
+ * Copyright (c) 2023, Tuomas Kontiainen
+ */
+package com.xahico.boot.pilot;
+
+import com.xahico.boot.synchronicity.ClockedIntensity;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Threading {
+	ClockedIntensity intensity () default ClockedIntensity.MEDIUM;
+	
+	Class<?>[] shareExecutorWith ();
+}
