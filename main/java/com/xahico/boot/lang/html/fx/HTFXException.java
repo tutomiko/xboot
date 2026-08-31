c869db03001d56a94a332b6d1f610dcc887453e4
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:10:17 +0300

    add: main/java/com/xahico/boot/lang/html/fx/HTFXException.java

---
+++ main/java/com/xahico/boot/lang/html/fx/HTFXException.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.xahico.boot.lang.html.fx;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public class HTFXException extends Exception {
+	public HTFXException (){
+		super();
+	}
+	
+	public HTFXException (final String message){
+		super(message);
+	}
+	
+	public HTFXException (final String message, final Throwable cause){
+		super(message, cause);
+	}
+}
