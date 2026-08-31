3ceb2ea4e487de1c4300c32e853a4c7c2e39f844
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:13:15 +0300

    add: main/java/com/xahico/boot/net/sock/model/asio/ASIOExchangeConfigurator.java

---
+++ main/java/com/xahico/boot/net/sock/model/asio/ASIOExchangeConfigurator.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.xahico.boot.net.sock.model.asio;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+@FunctionalInterface
+public interface ASIOExchangeConfigurator {
+	void configure (final ASIOExchange session);
+}
