2abe73e777b9cac3139883e1861f3eb90635c8ae
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:15:42 +0300

    add: main/java/com/xahico/boot/net/sock/model/lobex/LOBEXException.java

---
+++ main/java/com/xahico/boot/net/sock/model/lobex/LOBEXException.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.xahico.boot.net.sock.model.lobex;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public class LOBEXException extends Exception {
+	public LOBEXException (){
+		super();
+	}
+	
+	public LOBEXException (final String message){
+		super(message);
+	}
+}
