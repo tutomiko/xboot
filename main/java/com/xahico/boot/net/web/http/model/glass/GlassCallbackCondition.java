68b1c1fcff7fc86aa6c44a12e2941b1dabc9592e
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:17:41 +0300

    add: main/java/com/xahico/boot/net/web/http/model/glass/GlassCallbackCondition.java

---
+++ main/java/com/xahico/boot/net/web/http/model/glass/GlassCallbackCondition.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.xahico.boot.net.web.http.model.glass;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public enum GlassCallbackCondition {
+	FAILURE,
+	
+	SUCCESS,
+	
+	NONE,
+}
