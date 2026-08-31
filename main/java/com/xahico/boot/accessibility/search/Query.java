2646a4b2d521100e10a6bfa0fd71acf175db6966
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:07:55 +0300

    add: main/java/com/xahico/boot/accessibility/search/Query.java

---
+++ main/java/com/xahico/boot/accessibility/search/Query.java
@@ -0,0 +1,16 @@
+/*
+ * Written by Tuomas Kontiainen <https://www.github.com/tutomiko>
+ * 
+ * Copyright (c) 2023, Tuomas Kontiainen
+ */
+package com.xahico.boot.accessibility.search;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public final class Query {
+	public String        searchString;
+	public SortingPolicy sortingPolicy;
+}
