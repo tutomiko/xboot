89f8c7383a241723177901e99fd1c9f39f2aa8cd
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:15:44 +0300

    add: main/java/com/xahico/boot/net/sock/model/lobex/LOBEXMethod.java

---
+++ main/java/com/xahico/boot/net/sock/model/lobex/LOBEXMethod.java
@@ -0,0 +1,56 @@
+/*
+ * Written by Tuomas Kontiainen <https://www.github.com/tutomiko>
+ * 
+ * Copyright (c) 2023, Tuomas Kontiainen
+ */
+package com.xahico.boot.net.sock.model.lobex;
+
+import com.xahico.boot.reflection.ReflectionMethod;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * TBD.
+ * 
+ * @param <REQ> 
+ * TBD.
+ * 
+ * @param <RSP> 
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+final class LOBEXMethod <REQ extends LOBEXRequest, RSP extends LOBEXResponse> {
+	private final String           control;
+	private final ReflectionMethod methodHandle;
+	private final Class<REQ>       requestClass;
+	private final Class<RSP>       responseClass;
+	
+	
+	
+	LOBEXMethod (final ReflectionMethod methodHandle, final String control, final Class<REQ> requestClass, final Class<RSP> responseClass){
+		super();
+		
+		this.methodHandle = methodHandle;
+		this.control = control;
+		this.requestClass = requestClass;
+		this.responseClass = responseClass;
+	}
+	
+	
+	
+	public String getControl (){
+		return this.control;
+	}
+	
+	public Class<REQ> getRequestClass (){
+		return this.requestClass;
+	}
+	
+	public Class<RSP> getResponseClass (){
+		return this.responseClass;
+	}
+	
+	public void invoke (final Object instance, final REQ request, final RSP response) throws ExecutionException {
+		this.methodHandle.invoke(instance, request, response);
+	}
+}
