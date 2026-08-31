bf29355d8d1a08f26a9f7a6a0666f450b1fdc58e
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:22:31 +0300

    add: main/java/com/xahico/boot/publish/GWXStatus.java

---
+++ main/java/com/xahico/boot/publish/GWXStatus.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.xahico.boot.publish;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public enum GWXStatus {
+	AUTHORIZATION,
+	INVALID_LOCATION,
+	INVALID_PARAMETERS,
+	SUCCESS,
+	UNKNOWN,
+}
