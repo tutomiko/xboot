e2e3e240209877efd2b80e6f79d837795ea33e24
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:20:22 +0300

    add: main/java/com/xahico/boot/publish/collections/GWXListBackedReadOnlyNodeCollection.java

---
+++ main/java/com/xahico/boot/publish/collections/GWXListBackedReadOnlyNodeCollection.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.xahico.boot.publish.collections;
+
+import com.xahico.boot.publish.GWXNode;
+import com.xahico.boot.publish.GWXNodeCollection;
+import com.xahico.boot.publish.GWXUtilities;
+import com.xahico.boot.util.OrderedConsumer;
+import java.util.List;
+
+/**
+ * TBD.
+ * 
+ * @param <T> 
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public class GWXListBackedReadOnlyNodeCollection <T extends GWXNode> extends GWXReadOnlyNodeCollection<T> {
+	protected final List<T> nodes;
+	
+	
+	
+	public GWXListBackedReadOnlyNodeCollection (final List<T> nodes){
+		super();
+		
+		this.nodes = nodes;
+	}
+	
+	public GWXListBackedReadOnlyNodeCollection (final GWXNode owner, final List<T> nodes){
+		super(owner);
+		
+		this.nodes = nodes;
+	}
+	
+	
+	
+	@Override
+	public boolean contains (final T node){
+		return nodes.contains(node);
+	}
+	
+	@Override
+	public T lookup (final Object key){
+		for (final var node : nodes) {
+			if (GWXUtilities.checkNodeKey(node, key)) {
+				return node;
+			}
+		}
+
+		return null;
+	}
+	
+	@Override
+	public int size (){
+		return nodes.size();
+	}
+	
+	@Override
+	public void walk (final OrderedConsumer<T> consumer) {
+		for (final var node : nodes) {
+			if (! consumer.accept(node)) {
+				break;
+			}
+		}
+	}
+}
