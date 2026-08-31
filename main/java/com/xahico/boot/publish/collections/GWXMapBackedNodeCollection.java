f246c7f04ca12eefd2041fff5b21deeb90f4fc72
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:20:28 +0300

    add: main/java/com/xahico/boot/publish/collections/GWXMapBackedNodeCollection.java

---
+++ main/java/com/xahico/boot/publish/collections/GWXMapBackedNodeCollection.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.xahico.boot.publish.collections;
+
+import com.xahico.boot.publish.GWXNode;
+import com.xahico.boot.publish.GWXNodeCollection;
+import com.xahico.boot.publish.GWXUtilities;
+import com.xahico.boot.util.OrderedConsumer;
+import java.util.Map;
+
+/**
+ * TBD.
+ * 
+ * @param <T> 
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public class GWXMapBackedNodeCollection <T extends GWXNode> extends GWXNodeCollection<T> {
+	protected final Map<Object, T> nodes;
+	
+	
+	
+	public GWXMapBackedNodeCollection (final Map<Object, T> nodes){
+		super();
+		
+		this.nodes = nodes;
+	}
+	
+	public GWXMapBackedNodeCollection (final GWXNode owner, final Map<Object, T> nodes){
+		super(owner);
+		
+		this.nodes = nodes;
+	}
+	
+	
+	
+	@Override
+	public void add (final T node){
+		final T ov;
+
+		ov = nodes.put(GWXUtilities.getNodeId(node), node);
+
+		if (ov != node) {
+			this.bind(node);
+		}
+	}
+
+	@Override
+	public boolean contains (final T node){
+		return nodes.containsKey(GWXUtilities.getNodeId(node));
+	}
+
+	@Override
+	public T lookup (final Object key){
+		return nodes.get(key);
+	}
+
+	@Override
+	public void remove (final T node){
+		final Object key;
+		final T      ov;
+
+		key = GWXUtilities.getNodeId(node);
+
+		ov = nodes.remove(key);
+
+		if (ov == node) {
+			this.unbind(node);
+		} else {
+			nodes.put(key, ov);
+		}
+	}
+
+	@Override
+	public int size (){
+		return nodes.size();
+	}
+
+	@Override
+	public void walk (final OrderedConsumer<T> consumer) {
+		for (final var node : nodes.values()) {
+			if (! consumer.accept(node)) {
+				break;
+			}
+		}
+	}
+}
