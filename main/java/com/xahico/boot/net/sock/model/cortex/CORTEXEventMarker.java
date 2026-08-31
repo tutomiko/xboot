6b150c18e7500ca25836d4f61400c9651164006d
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:14:55 +0300

    add: main/java/com/xahico/boot/net/sock/model/cortex/CORTEXEventMarker.java

---
+++ main/java/com/xahico/boot/net/sock/model/cortex/CORTEXEventMarker.java
@@ -0,0 +1,24 @@
+/*
+ * Written by Tuomas Kontiainen <https://www.github.com/tutomiko>
+ * 
+ * Copyright (c) 2023, Tuomas Kontiainen
+ */
+package com.xahico.boot.net.sock.model.cortex;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface CORTEXEventMarker {
+	String value ();
+}
