9f1988ef87eab73b2e277fe6265fc67b24353017
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:18:21 +0300

    add: main/java/com/xahico/boot/net/web/http/model/glass/GlassPingEvent.java

---
+++ main/java/com/xahico/boot/net/web/http/model/glass/GlassPingEvent.java
@@ -0,0 +1,16 @@
+/*
+ * Written by Tuomas Kontiainen <https://www.github.com/tutomiko>
+ * 
+ * Copyright (c) 2023, Tuomas Kontiainen
+ */
+package com.xahico.boot.net.web.http.model.glass;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+@GlassEventMarkup()
+public final class GlassPingEvent extends GlassEvent {
+	
+}
