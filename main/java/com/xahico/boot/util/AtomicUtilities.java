33dc83bb6418e42e3cbf3e6ada6dc06dccdceb9f
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:23:52 +0300

    add: main/java/com/xahico/boot/util/AtomicUtilities.java

---
+++ main/java/com/xahico/boot/util/AtomicUtilities.java
@@ -0,0 +1,17 @@
+/*
+ * Written by Tuomas Kontiainen <https://www.github.com/tutomiko>
+ * 
+ * Copyright (c) 2023, Tuomas Kontiainen
+ */
+package com.xahico.boot.util;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public final class AtomicUtilities {
+	private AtomicUtilities (){
+		throw new UnsupportedOperationException("Not supported.");
+	}
+}
