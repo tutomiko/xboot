0c37bfb11b89d815faba929f3163a74578e93815
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:08:58 +0300

    add: main/java/com/xahico/boot/event/Event.java

---
+++ main/java/com/xahico/boot/event/Event.java
@@ -0,0 +1,15 @@
+/*
+ * Written by Tuomas Kontiainen <https://www.github.com/tutomiko>
+ * 
+ * Copyright (c) 2023, Tuomas Kontiainen
+ */
+package com.xahico.boot.event;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public interface Event {
+	
+}
