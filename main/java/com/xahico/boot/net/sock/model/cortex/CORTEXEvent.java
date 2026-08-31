bbdfcd0f401ea04e546a4f0e838b47c8f91ad15f
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:14:53 +0300

    add: main/java/com/xahico/boot/net/sock/model/cortex/CORTEXEvent.java

---
+++ main/java/com/xahico/boot/net/sock/model/cortex/CORTEXEvent.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.xahico.boot.net.sock.model.cortex;
+
+import com.xahico.boot.lang.jsox.JSOXObject;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public abstract class CORTEXEvent extends JSOXObject {
+	
+}
