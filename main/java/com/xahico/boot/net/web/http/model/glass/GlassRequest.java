6425f762eadec5425bd625f1bbfd959fd70086a1
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:18:23 +0300

    add: main/java/com/xahico/boot/net/web/http/model/glass/GlassRequest.java

---
+++ main/java/com/xahico/boot/net/web/http/model/glass/GlassRequest.java
@@ -0,0 +1,17 @@
+/*
+ * Written by Tuomas Kontiainen <https://www.github.com/tutomiko>
+ * 
+ * Copyright (c) 2023, Tuomas Kontiainen
+ */
+package com.xahico.boot.net.web.http.model.glass;
+
+import com.xahico.boot.lang.jsox.JSOXObject;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public abstract class GlassRequest extends JSOXObject {
+	
+}
