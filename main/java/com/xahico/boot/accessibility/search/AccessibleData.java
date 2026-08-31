1d8dfebd9024401c9f32a8038dd2676b33c063a2
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:07:41 +0300

    add: main/java/com/xahico/boot/accessibility/search/AccessibleData.java

---
+++ main/java/com/xahico/boot/accessibility/search/AccessibleData.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.xahico.boot.accessibility.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public final class AccessibleData {
+	final List<Matcher> matcherList = new ArrayList<>();
+	
+	
+	
+	AccessibleData (){
+		super();
+	}
+}
