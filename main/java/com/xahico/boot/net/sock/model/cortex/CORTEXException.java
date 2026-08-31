d8bdddc0ba10e4eb3aa05cdccdf5003cd170377b
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:14:57 +0300

    add: main/java/com/xahico/boot/net/sock/model/cortex/CORTEXException.java

---
+++ main/java/com/xahico/boot/net/sock/model/cortex/CORTEXException.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.xahico.boot.net.sock.model.cortex;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public class CORTEXException extends Exception {
+	public CORTEXException (){
+		super();
+	}
+	
+	public CORTEXException (final String message){
+		super(message);
+	}
+}
