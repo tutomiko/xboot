4c094889cb4e7237cc57f05ec4020df6c28847a5
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:08:04 +0300

    add: main/java/com/xahico/boot/accessibility/search/QueryResults.java

---
+++ main/java/com/xahico/boot/accessibility/search/QueryResults.java
@@ -0,0 +1,21 @@
+/*
+ * Written by Tuomas Kontiainen <https://www.github.com/tutomiko>
+ * 
+ * Copyright (c) 2023, Tuomas Kontiainen
+ */
+package com.xahico.boot.accessibility.search;
+
+import java.util.List;
+
+/**
+ * TBD.
+ * 
+ * @param <T> 
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public final class QueryResults <T extends Accessible> {
+	public List<T> collection = null;
+	public int     totalResults = 0;
+}
