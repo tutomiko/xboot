e25d36dea5c1151af4d20678759ee501a1fa6cff
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:22:07 +0300

    add: main/java/com/xahico/boot/publish/GWXPropertyAccessor.java

---
+++ main/java/com/xahico/boot/publish/GWXPropertyAccessor.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.xahico.boot.publish;
+
+/**
+ * TBD.
+ * 
+ * @param <T> 
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+@FunctionalInterface
+public interface GWXPropertyAccessor <T> {
+	T get ();
+}
