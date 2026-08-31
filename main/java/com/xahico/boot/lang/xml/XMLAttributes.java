b8acd47414e515f2cd9343d933361f9ef01af08b
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:12:03 +0300

    add: main/java/com/xahico/boot/lang/xml/XMLAttributes.java

---
+++ main/java/com/xahico/boot/lang/xml/XMLAttributes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.xahico.boot.lang.xml;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public final class XMLAttributes {
+	public XMLAttributes (){
+		super();
+	}
+}
