4139824970ce487e4669daeeea258ffd9aa23838
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:19:24 +0300

    add: main/java/com/xahico/boot/pilot/LookupException.java

---
+++ main/java/com/xahico/boot/pilot/LookupException.java
@@ -0,0 +1,24 @@
+/*
+ * Written by Tuomas Kontiainen <https://www.github.com/tutomiko>
+ * 
+ * Copyright (c) 2023, Tuomas Kontiainen
+ */
+package com.xahico.boot.pilot;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public class LookupException extends Exception {
+	/**
+	 * Constructs an instance of <code>LookupException</code> with the
+	 * specified detail message.
+	 *
+	 * @param msg 
+	 * the detail message.
+	**/
+	public LookupException (final String msg){
+		super(msg);
+	}
+}
