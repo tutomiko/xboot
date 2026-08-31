b55161d947a7dbb61a638320584140472afe1b64
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:14:19 +0300

    add: main/java/com/xahico/boot/net/sock/model/bare/BARETransactResponse.java

---
+++ main/java/com/xahico/boot/net/sock/model/bare/BARETransactResponse.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.xahico.boot.net.sock.model.bare;
+
+import com.xahico.boot.lang.jsox.JSOXObject;
+import com.xahico.boot.lang.jsox.JSOXVariant;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+final class BARETransactResponse extends JSOXObject {
+	public JSOXVariant response;
+	public String      transactionId;
+}
