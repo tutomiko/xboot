df96b5377929023d8114378c1000560c3d8c3b26
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:13:30 +0300

    add: main/java/com/xahico/boot/net/sock/model/asio/ASIOResponse.java

---
+++ main/java/com/xahico/boot/net/sock/model/asio/ASIOResponse.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.xahico.boot.net.sock.model.asio;
+
+import com.xahico.boot.lang.jsox.JSOXObject;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public abstract class ASIOResponse extends JSOXObject {
+	
+}
