6a369c82c74c391abba218bce344122457b3ec7b
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:23:37 +0300

    add: main/java/com/xahico/boot/synchronicity/Watcher.java

---
+++ main/java/com/xahico/boot/synchronicity/Watcher.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.xahico.boot.synchronicity;
+
+/**
+ * TBD.
+ * 
+ * @param <T> 
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public interface Watcher <T> {
+	void onAdded (final T target);
+	
+	void onChanged (final T target);
+	
+	void onRemoved (final T target);
+}
