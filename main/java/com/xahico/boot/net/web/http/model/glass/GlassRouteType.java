cddc03d6d6d6c1fb3a38823321faa79c8e45345b
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:18:31 +0300

    add: main/java/com/xahico/boot/net/web/http/model/glass/GlassRouteType.java

---
+++ main/java/com/xahico/boot/net/web/http/model/glass/GlassRouteType.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.xahico.boot.net.web.http.model.glass;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public enum GlassRouteType {
+	LOCATION,
+	RESOURCE,
+}
