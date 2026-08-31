2d1936d6ce1374b69ee539b6df0f178a7d067be0
tutomiko <98809576+tutomiko@users.noreply.github.com>
2026-08-31 17:23:30 +0300

    add: main/java/com/xahico/boot/synchronicity/Synchronization.java

---
+++ main/java/com/xahico/boot/synchronicity/Synchronization.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.xahico.boot.synchronicity;
+
+/**
+ * TBD.
+ * 
+ * @author Tuomas Kontiainen
+**/
+public final class Synchronization {
+	private Synchronization (){
+		throw new UnsupportedOperationException("Not supported.");
+	}
+}
